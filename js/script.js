117cbfe9a380605a262dc1453e78fc6167b8a18c
nadiyaivana <nadiya@klik.digital>
2023-02-23 11:46:47 +0700

    form reset, header responsive

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-/////////////// Welcoming speech ///////////////  
+/////////////// Welcoming speech ///////////////
 // var nama = prompt("What's your name?");
 // document.getElementById("nama").innerText = nama;
 
 ////////////// Get today date and time //////////////
 var dt = new Date();
-document.getElementById('dt').innerHTML=dt;
+document.getElementById("dt").innerHTML = dt;
 
 ////////////// Form //////////////
 function validateForm() {
-    var name = document.forms["message-form"]["name"].value;
-    var date = document.forms["message-form"]["date"].value;
-    var gender = document.forms["message-form"]["gender"].value;
-    var messages = document.forms["message-form"]["message"].value; 
+  var name = document.forms["message-form"]["name"].value;
+  var date = document.forms["message-form"]["date"].value;
+  var gender = document.forms["message-form"]["gender"].value;
+  var messages = document.forms["message-form"]["message"].value;
 
-    if (name == "" || date == "" || gender == "" || messages ==""){
-        alert("Field cannot be empty!");
-        return false;
-    }
-    document.getElementById("output-name").innerText = name;
-    document.getElementById("output-date").innerText = date;
-    document.getElementById("output-gender").innerText = gender;
-    document.getElementById("output-message").innerText = messages;
-    
+  if (name == "" || date == "" || gender == "" || messages == "") {
+    alert("Field cannot be empty!");
     return false;
+  }
+  document.getElementById("output-name").innerText = name;
+  document.getElementById("output-date").innerText = date;
+  document.getElementById("output-gender").innerText = gender;
+  document.getElementById("output-message").innerText = messages;
+
+  document.getElementsByName("message-form")[0].reset();
+  return false;
+}
+
+//////////////// Responsive header //////////////////
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
